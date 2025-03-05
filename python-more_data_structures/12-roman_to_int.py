c0e2a4a2538545b4ac81ef92df19c9682c96022c
cmalaikat16 <c.malaikat@alustudent.com>
2025-03-05 08:08:16 -0800

    ese we

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,21 +9,6 @@ def roman_to_int(roman_string):
     next_numeral = 0
     length = len(roman_string)
     while (i < length):
-        current_numeral = numerals[roma#!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
-root@4cedb538bf77:/alu-higher_level_programming/python-more_data_structures# cat 12-roman_to_int.py 
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        return (0)
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    i = 0
-    next_numeral = 0
-    length = len(roman_string)
-    while (i < length):
         current_numeral = numerals[roman_string[i]]
         if i + 1 <= length - 1:
             next_numeral = numerals[roman_string[i+1]]

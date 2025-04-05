6c421fa6c30191dcd59cd7f5946a84e7eb60f688
cmalaikat16 <c.malaikat@alustudent.com>
2025-04-05 06:24:59 +0000

    more

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-# python script that fetches 'https://alu-intranet.hbtn.io/status'
-"""making request to provided url"""
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+if _name_ == '_main_':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,18 +1,20 @@
-!/usr/bin/python3
-""" Script that takes in a URL and an email, sends a POST request
- And displays the body of the response
+#!/usr/bin/python3
+"""
+Module 2-post_email.py
 """
 
-import sys
+
 import urllib.parse
 import urllib.request
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as resp:
-        print(resp.read().decode("utf-8"))
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read().decode('utf-8')
+        print(page)
